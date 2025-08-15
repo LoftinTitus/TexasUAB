08b7d6a276e44fdf24447d685c27b1f09e8cd3e2
Ty Loftin <tyloftin4@gmail.com>
2025-08-15 15:10:20 -0500

    debug outputs

--- main.tsx
+++ main.tsx
@@ -3,7 +3,11 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/globals.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+console.log('main.tsx loaded');
+const rootElement = document.getElementById('root');
+console.log('Root element:', rootElement);
+
+ReactDOM.createRoot(rootElement!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
